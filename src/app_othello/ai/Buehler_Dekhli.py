c26714b6f4fb75b7628e7adcc9104e85ff6ff80b
ylked <nima@dekhli.ch>
2023-12-15 15:20:43 +0100

    use alpha-beta

--- src/app_othello/ai/Buehler_Dekhli.py
+++ src/app_othello/ai/Buehler_Dekhli.py
@@ -61,6 +61,52 @@ class Node:
         return max_node, max_op
 
 
+    def ab_min(self, parent_max, depth) -> (Node, (int, int)):
+        if self.final() or depth <= 0:
+            return self, None
+
+        min_node = None
+        min_op = None
+
+        for row, col in self.operators():
+            new_game = self.game.copy_game()
+            new_game.move(row, col)
+            new_node = Node(new_game)
+            res, _ = new_node.ab_max(min_node, depth-1)
+
+            if min_node is None or res.eval() < min_node.eval():
+                min_op = (row, col)
+                min_node = res
+
+                if min_node is not None and parent_max is not None:
+                    if min_node.eval() < parent_max.eval():
+                        break
+
+        return min_node, min_op
+
+    def ab_max(self, parent_min, depth) -> (Node, (int, int)):
+        if self.final() or depth <= 0:
+            return self, None
+
+        max_node = None
+        max_op = None
+
+        for row, col in self.operators():
+            new_game = self.game.copy_game()
+            new_game.move(row, col)
+            new_node = Node(new_game)
+            res, _ = new_node.ab_min(max_node, depth-1)
+
+            if max_node is None or res.eval() > max_node.eval():
+                max_op = (row, col)
+                max_node = res
+
+                if max_node is not None and parent_min is not None:
+                    if max_node.eval() > parent_min.eval():
+                        break
+
+        return max_node, max_op
+
 
 class Buehler_Dekhli:
     '''The name of this class must be the same as its file.
@@ -82,7 +128,7 @@ class Buehler_Dekhli:
         color = board.get_turn()
 
         node = Node(board)
-        _, op = node.max(3)
+        _, op = node.ab_max(None, 5)
         return op
 
     def __str__(self):
