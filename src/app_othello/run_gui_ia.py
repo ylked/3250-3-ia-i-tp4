9e01dcb96ea7267885d6172abdad1d51cc56e816
ylked <nima@dekhli.ch>
2024-01-06 06:27:07 +0100

    add parallel games function

--- src/app_othello/run_gui_ia.py
+++ src/app_othello/run_gui_ia.py
@@ -3,6 +3,28 @@ Run this script if you want your IA to automatically play against Random
 without having to change game settings.
 """
 
+import multiprocessing
 import othello_gui
-game = othello_gui.OthelloGUI('Buehler_Dekhli_v1', 'Buehler_Dekhli_v4')
-game.run_auto()
+
+base_name = "Buehler_Dekhli_"
+runners = (
+    (base_name + "v1", base_name + "v5"),
+    (base_name + "v2", base_name + "v5"),
+    (base_name + "v4", base_name + "v5"),
+    ("Random", base_name + "v5")
+)
+
+
+def run(black, white):
+    _game = othello_gui.OthelloGUI(black, white)
+    _game.run_auto()
+
+
+def run_tests():
+    for first, second in runners:
+        multiprocessing.Process(target=run, args=(first, second)).start()
+        multiprocessing.Process(target=run, args=(second, first)).start()
+
+
+if __name__ == "__main__":
+    run_tests()
